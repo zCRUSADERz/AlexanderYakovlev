0e3f49310b64b5c1ea06120e17b32382ba2fc4a7
zCRUSADERz <sanyakovlev@yandex.ru>
2017-12-29 23:05:52 +0700

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+ * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
+ * @since 29.12.2017
+ * @version 1.0
+ */
 public class Calculate {
+	
+	/**
+	 * Вывод строки в консоль
+	 * @param arg - arg.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
