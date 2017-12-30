c035ef836e911e69ae7cd4c9ab0e55208a834e2f
zCRUSADERz <sanyakovlev@yandex.ru>
2017-12-30 20:16:29 +0700

    2.3. Элементарный калькулятор.

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,57 @@
+package ru.job4j.calculator;
+
+/**
+ * Calculator
+ *
+ * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
+ * @since 30.12.2017
+ * @version 1.0
+ *
+ *
+ */
+public class Calculator {
+    private double result;
+
+    /**
+     * First add second
+     * @param first - first num
+     * @param second - second num
+     */
+    void add(double first, double second) {
+        result = first + second;
+    }
+
+    /**
+     * First minus second
+     * @param first - first num
+     * @param second - second num
+     */
+    void subtract(double first, double second) {
+        result = first - second;
+    }
+
+    /**
+     * First divide second
+     * @param first - first num
+     * @param second - second num
+     */
+    void div(double first, double second) {
+        result = first / second;
+    }
+
+    /**
+     * First multiply second
+     * @param first - first num
+     * @param second - second num
+     */
+    void multiple(double first, double second) {
+        result = first * second;
+    }
+
+    /**
+     * @return result;
+     */
+    double getResult() {
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Calculator test.
+ */
+public class CalculatorTest {
+
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOnePlusTwoThenResultThree() {
+        Calculator calc = new Calculator();
+        calc.add(1, 2);
+        double result = calc.getResult();
+        double expected = 3.0;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test subtract.
+     */
+    @Test
+    public void whenSubtractTwoMinusOneThenResultOne() {
+        Calculator calc = new Calculator();
+        calc.subtract(2, 1);
+        double result = calc.getResult();
+        double expected = 1.0;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test div.
+     */
+    @Test
+    public void whenDivFourDivideTwoThenResultTwo() {
+        Calculator calc = new Calculator();
+        calc.div(4, 2);
+        double result = calc.getResult();
+        double expected = 2.0;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test multiple.
+     */
+    @Test
+    public void whenMultipleTwoMultiplyTwoThenResultFour() {
+        Calculator calc = new Calculator();
+        calc.multiple(2, 2);
+        double result = calc.getResult();
+        double expected = 4.0;
+        assertThat(result, is(expected));
+    }
+}
