34bbda74ff9bfaf1df24382a361fe78476c61b68
zCRUSADERz <sanyakovlev@yandex.ru>
2017-12-31 12:51:27 +0700

    3.3. Вычисление площади треугольника.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,59 @@
+package ru.job4j.condition;
+
+import java.math.BigDecimal;
+
+/**
+ * triangle.
+ * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
+ * @since 31.12.2017
+ * @version 1.0
+ */
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Semiperimeter.
+     * @param ab - distance between points a and b.
+     * @param ac - distance between points a and c.
+     * @param bc - distance between points b and c.
+     * @return - semiperimitr.
+     */
+    private double semiperimeter(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+
+    /**
+     * Area of triangle.
+     * @return - area.
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        if (this.exist(ab, ac, bc)) {
+            double p = this.semiperimeter(ab, ac, bc);
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+    /**
+     * Existence of a triangle.
+     * @param ab - distance between points a and b.
+     * @param ac - distance between points a and c.
+     * @param bc - distance between points b and c.
+     * @return - true if exist.
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        return ((ab + ac) > bc) && ((ab + bc) > ac) && ((ac + bc) > ab);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Triangle test.
+ * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
+ * @since 31.12.2017
+ * @version 1.0
+ */
+public class TriangleTest {
+
+    /**
+     * Test Triangle.
+     */
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 2.0;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+
+    /**
+     * Test non-existent triangle.
+     */
+    @Test
+    public void whenAreaSetThreePointsOnOneStraightLineThenTriangleArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 2);
+        Point c = new Point(10, 10);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = -1.0;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
