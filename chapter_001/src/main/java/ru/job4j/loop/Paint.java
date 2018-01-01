fdeb51a286fed86803a53d320d3633cf2ea81a07
zCRUSADERz <sanyakovlev@yandex.ru>
2018-01-01 23:15:23 +0700

    4.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,27 +1,63 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
- * Paint.
+ * Paint pyramid.
  * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
  * @since 1.01.2018
  * @version 1.0
  */
 public class Paint {
 
+    /**
+     * Paint right triangle of the pyramid.
+     * @param height - height of the pyramid.
+     * @return - right triangle of the pyramid.
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column);
+    }
+
+    /**
+     * Paint left triangle of the pyramid.
+     * @param height - height of the pyramid.
+     * @return - left triangle of the pyramid.
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1);
+    }
+
+    /**
+     * Paint pyramid.
+     * @param height - height of the pyramid.
+     * @return - pyramid.
+     */
     public String pyramid(int height) {
-        StringBuilder pyramid = new StringBuilder();
-        String ln = System.lineSeparator();
-        int weight = 2 * height - 1;
-        for (int row = 0; row < height; row++) {
-            for (int column = 0; column < weight; column++) {
-                if (row >= (height - column - 1) && (row + height - 1) >= column) {
-                    pyramid.append("^");
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column);
+    }
+
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predict) {
+        StringBuilder screen = new StringBuilder();
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (predict.test(row, column)) {
+                    screen.append("^");
                 } else {
-                    pyramid.append(" ");
+                    screen.append(" ");
                 }
             }
-            pyramid.append(ln);
+            screen.append(System.lineSeparator());
         }
-        return pyramid.toString();
+        return screen.toString();
     }
 }
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -7,12 +7,38 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * Paint test.
+ * Paint pyramid test.
  * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
  * @since 1.01.2018
  * @version 1.0
  */
 public class PaintTest {
+
+    @Test
+    public void whenPyramidHeightFourThenRightTriangleOfThePyramid() {
+        Paint paint = new Paint();
+        String result = paint.rightTrl(4);
+        String expected = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("^   ")
+                .add("^^  ")
+                .add("^^^ ")
+                .add("^^^^")
+                .toString();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenPyramidHeightFourThenLeftTriangleOfThePyramid() {
+        Paint paint = new Paint();
+        String result = paint.leftTrl(4);
+        String expected = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("   ^")
+                .add("  ^^")
+                .add(" ^^^")
+                .add("^^^^")
+                .toString();
+        assertThat(result, is(expected));
+    }
 
     @Test
     public void whenPyramidHeightFour() {
