9d757afcbea4f7fb452ad75ea151cbc756748234
zCRUSADERz <sanyakovlev@yandex.ru>
2018-02-02 20:25:19 +0700

    5.3.2. Создать контейнер на базе связанного списка.

---
+++ chapter_004/src/main/java/ru/job4j/list/LinkedList.java
@@ -0,0 +1,178 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Linked list.
+ *
+ * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
+ * @since 02.02.2017
+ */
+public class LinkedList<E> implements Iterable<E> {
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+    /**
+     * Structurally modification count.
+     */
+    private int modCount;
+
+    LinkedList() {
+        first = null;
+        last = null;
+        size = 0;
+        modCount = 0;
+    }
+
+    /**
+     * Add element to the end.
+     * @param value - element.
+     */
+    public void add(E value) {
+        Node<E> previous = last;
+        Node<E> newNode = new Node<>(previous, value, null);
+        last = newNode;
+        if (first == null) {
+            first = newNode;
+        } else {
+            previous.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    /**
+     * Returns the element at the index position.
+     * @param index - position.
+     * @return - element at the index position.
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        Node<E> result;
+        if (index < (size >> 1)) {
+            Node<E> node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            result = node;
+        } else {
+            Node<E> node = last;
+            for (int i = size - 1; i > index; i--) {
+                node = node.previous;
+            }
+            result = node;
+        }
+        return result.item;
+    }
+
+    /**
+     * Remove node and restores links between nodes.
+     * @param node - removable node.
+     */
+    private void removeNode(Node<E> node) {
+        Node<E> next = node.next;
+        Node<E> previous = node.previous;
+
+        if (previous == null) {
+            first = next;
+        } else {
+            previous.next = next;
+        }
+        if (next == null) {
+            last = previous;
+        } else {
+            next.previous = previous;
+        }
+
+        node.item = null;
+        node.previous = null;
+        node.next = null;
+
+        size--;
+        modCount++;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> previous;
+
+        Node(Node<E> previous, E item, Node<E> next) {
+            this.previous = previous;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private class Itr implements Iterator<E> {
+        private Node<E> next;
+        private Node<E> lastReturned;
+        private int cursor;
+        private int expectedModCount;
+
+        Itr() {
+            next = first;
+            lastReturned = null;
+            cursor = 0;
+            expectedModCount = modCount;
+        }
+
+        /**
+         * Returns true if the iteration has more element.
+         * @return - true, if has next element.
+         */
+        @Override
+        public boolean hasNext() {
+            return cursor < size;
+        }
+
+        /**
+         * Returns the next element.
+         * @return - next element.
+         */
+        @Override
+        public E next() {
+            checkModification();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            lastReturned = next;
+            next = next.next;
+            cursor++;
+            return lastReturned.item;
+        }
+
+        /**
+         * Removes the last element returned by this iterator.
+         */
+        @Override
+        public void remove() {
+            checkModification();
+            if (lastReturned == null) {
+                throw new IllegalStateException();
+            }
+            removeNode(lastReturned);
+            cursor--;
+            lastReturned = null;
+            expectedModCount++;
+        }
+
+        /**
+         * If the list has been changed, throws ConcurrentModificationException.
+         */
+        private void checkModification() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+        }
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/list/LinkedListTest.java
@@ -0,0 +1,129 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Linked list.
+ *
+ * @author Alexander Yakovlev (sanyakovlev@yandex.ru)
+ * @since 02.02.2017
+ */
+public class LinkedListTest {
+    private LinkedList<String> list;
+
+    @Before
+    public void setList() {
+        list = new LinkedList<>();
+        list.add("0");
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+    }
+
+    @Test
+    public void whenAddAndGetElementThenReturnThisElement() {
+        String result = list.get(1);
+        assertThat(result, is("1"));
+    }
+
+    @Test
+    public void whenAddSeveralElementsAndGetLastElementThenReturnThisElement() {
+        list.add("1");
+        list.add("2");
+        String str = "3";
+        list.add(str);
+        String result = list.get(4);
+        assertThat(result, is("4"));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenGetExistElementThenIndexOutOfBoundException() {
+        list.get(10);
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenGetByNegativeIndexThenIndexOutOfBoundException() {
+        list.get(-1);
+    }
+
+    @Test
+    public void whenAddAndIteratorHasNextThenReturnTrue() {
+        Iterator<String> it = list.iterator();
+        boolean result = it.hasNext();
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenIteratorHasNextThenReturnFalse() {
+        list = new LinkedList<>();
+        Iterator<String> it = list.iterator();
+        boolean result = it.hasNext();
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenAddAndIteratorNextElementThenReturnThisElement() {
+        Iterator<String> it = list.iterator();
+        String result = it.next();
+        assertThat(result, is("0"));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenListEmptyAndIteratorNextThenNoSuchElementException() {
+        list = new LinkedList<>();
+        list.iterator().next();
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void whenIteratorRemoveThenException() {
+        new LinkedList<>().iterator().remove();
+    }
+
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenGetIteratorAndChangeListThenRemoveThrowsException() {
+        Iterator<String> it = list.iterator();
+        list.add("");
+        it.remove();
+    }
+
+    @Test
+    public void whenIteratorRemoveFristThenNextReturnNext() {
+        Iterator<String> it = list.iterator();
+        it.next();
+        it.remove();
+        String result = it.next();
+        assertThat(result, is("1"));
+    }
+
+    @Test
+    public void whenIteratorRemoveFromTheMiddleThenNextReturnNext() {
+        Iterator<String> it = list.iterator();
+        it.next();
+        it.next();
+        it.remove();
+        String result = it.next();
+        assertThat(result, is("2"));
+    }
+
+    @Test
+    public void whenIteratorRemoveLastThenHasNextReturnFalse() {
+        Iterator<String> it = list.iterator();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        it.remove();
+        boolean result = it.hasNext();
+        assertThat(result, is(false));
+    }
+}
